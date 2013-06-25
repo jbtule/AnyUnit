8b4a1ef57660516c2da0faba846ab63f7e4b3908
jbtule <jay+code@tuley.name>
2013-06-25 12:02:59 -0500

    Fixed issue with extracted path being incorrect

--- .ci/install_portable_refs.sh
+++ .ci/install_portable_refs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Downloading Portable Profiles..."
 wget $DL_PORTABLE_REFS > /dev/null 2>&1
-unzip NETPortable.zip -d xbuild-frameworks
+unzip NETPortable.zip -d .ci/xbuild-frameworks
