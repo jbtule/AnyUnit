1047011a8a1f80d87ae09674ac72ccbc8b1a1d69
jbtule <jay+code@tuley.name>
2013-07-22 11:26:50 -0500

    Another unused non Profile1 namespace for Win2008CI

--- Contrib/PclUnit.Style.Xunit/Exceptions/AssertException.cs
+++ Contrib/PclUnit.Style.Xunit/Exceptions/AssertException.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.Serialization;
-using System.Security;
 
 /*
  * Copyright 2013 Outercurve Foundation
