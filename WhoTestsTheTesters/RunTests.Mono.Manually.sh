fe81fce01147fc6c096e5f07aa565a8241b754b6
jbtule <jay+code@tuley.name>
2013-08-07 20:47:18 -0500

    Added NunitTest to mono ci

--- WhoTestsTheTesters/RunTests.Mono.Manually.sh
+++ WhoTestsTheTesters/RunTests.Mono.Manually.sh
@@ -4,10 +4,11 @@ BASEDIR=$(dirname $0)
 TESTDLL1="$BASEDIR/BasicTests/bin/Debug/BasicTests.dll"
 TESTDLL2="$BASEDIR/ConstraintsTests/bin/Debug/ConstraintsTests.dll"
 TESTDLL3="$BASEDIR/Style/XunitTests/bin/Debug/XunitTests.dll"
+TESTDLL4="$BASEDIR/Style/NunitTests/bin/Debug/NunitTests.dll"
 
 
 mkdir mono-results
-mono "$BASEDIR/../Runner/Platforms/net/net40/bin/x86/Debug/net-40-x86-full.exe" run -o mono-results/net40-x86.json $TESTDLL1 $TESTDLL2 $TESTDLL3
-mono "$BASEDIR/../Runner/Platforms/net/net45/bin/x86/Debug/net-45-x86-full.exe" run -o mono-results/net45-x86.json $TESTDLL1 $TESTDLL2 $TESTDLL3
+mono "$BASEDIR/../Runner/Platforms/net/net40/bin/x86/Debug/net-40-x86-full.exe" run -o mono-results/net40-x86.json $TESTDLL1 $TESTDLL2 $TESTDLL3 $TESTDLL4
+mono "$BASEDIR/../Runner/Platforms/net/net45/bin/x86/Debug/net-45-x86-full.exe" run -o mono-results/net45-x86.json $TESTDLL1 $TESTDLL2 $TESTDLL3 $TESTDLL4
 
 mono "$BASEDIR/ConventionTestProcessor/bin/Debug/ConventionTestProcessor.exe" mono-results/*.json
