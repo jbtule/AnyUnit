956414d89f68a8f9467d73088c219d9f406dfe71
jbtule <jay+code@tuley.name>
2014-03-31 20:54:12 -0500

    fakemake build

---
+++ tools/makefile.fsx
@@ -0,0 +1,98 @@
+#I "packages/FAKE/tools"
+#r "FakeLib.dll"
+
+open Fake
+
+let projFiles =
+  !! "./PclUnit/*.csproj" //PclUnit
+    ++ "./Contrib/PclUnit.*/*.*proj" //Styles
+    ++ "./Runner/pclunit-runner/*.csproj" //AggregateRunner
+    ++ "./Runner/Platforms/**/*.csproj" //platform runners
+    -- if isMono then
+          "./Runner/Platforms/sl/**/*.csproj" //sliverlight runners
+       else
+          "./runner/Platforms/mono*/**/*.csproj" //mono runners
+    ++ "./WhoTestsTheTesters/**/*.*proj"
+    -- if isMono then
+          "./WhoTestsTheTesters/Tests/Silverlight*/*.csproj"
+       else
+          "./WhoTestsTheTesters/Tests/Mono*/*.csproj"
+
+
+Target "Clean" (fun () ->
+    trace " --- Cleaning stuff --- "
+
+    let buildMode = getBuildParamOrDefault "buildMode" "Release"
+    let setParams defaults =
+            { defaults with
+                Verbosity = Some(Quiet)
+                Targets = ["Clean"]
+                Properties =
+                    [
+                        "Optimize", "True"
+                        "DebugSymbols", "True"
+                        "Configuration", buildMode
+                        "FSharpBad", isMono.ToString()
+                    ]
+             }
+    projFiles
+      |> Seq.iter (build setParams)
+
+)
+
+Target "Build" (fun () ->
+    trace " --- Building the app --- "
+    let buildMode = getBuildParamOrDefault "buildMode" "Release"
+    let setParams defaults =
+            { defaults with
+                Verbosity = Some(Quiet)
+                Targets = ["Build"]
+                Properties =
+                    [
+                        "Optimize", "True"
+                        "DebugSymbols", "True"
+                        "Configuration", buildMode
+                        "FSharpBad", isMono.ToString()
+                    ]
+             }
+
+    projFiles
+      |> Seq.iter (build setParams)
+
+)
+
+Target "Test" (fun () ->
+    trace " --- Testing app --- "
+
+
+
+    let args =
+      sprintf
+        "runConfig ./WhoTestsTheTesters/Tests/RunTests%s.yml -o test.json --noerror"
+        (if isMono then ".mono" else "")
+
+    directExec (fun info ->
+                       info.FileName <- "./Runner/pclunit-runner/bin/Release/pclunit-runner.exe"
+                       info.Arguments <- args) |> ignore
+
+    if not directExec (fun info ->
+                       info.FileName <- "./WhoTestsTheTesters/ConventionTestProcessor/bin/Release/ConventionTestProcessor.exe"
+                       info.Arguments <- "test.json") then
+       failwith "Tests Failed"
+
+)
+
+Target "RestorePackages" RestorePackages
+
+Target "Deploy" (fun () ->
+    trace " --- Deploying app --- "
+)
+
+"Clean"
+  ==> "RestorePackages"
+  ==> "Build"
+  ==> "Test"
+  ==> "Deploy"
+
+
+RunTargetOrDefault "Deploy"
