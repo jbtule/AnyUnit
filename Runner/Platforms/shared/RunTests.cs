c2710dd797823162b5a5c3a06a9790b042f4c696
jbtule <jay+code@tuley.name>
2017-07-10 21:12:55 -0500

    better exception message

--- Runner/Platforms/shared/RunTests.cs
+++ Runner/Platforms/shared/RunTests.cs
@@ -55,106 +55,125 @@ namespace SatelliteRunner.Shared
             return file;
         }
 
-		public string PostHttp(string baseUri, string api, string json){
+        public string PostHttp(string baseUri, string api, string json){
 
-			var url = baseUri + api;
-			var byteArray = Encoding.UTF8.GetBytes(json);
+            var url = baseUri + api;
+            var byteArray = Encoding.UTF8.GetBytes(json);
 
-			var request = WebRequest.Create(url);
-			request.Method = "POST";
-			request.ContentLength = byteArray.Length;
-			request.ContentType = @"application/json";
+            var request = WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentLength = byteArray.Length;
+            request.ContentType = @"application/json";
 
-			var reqTask = Task.Factory.FromAsync<Stream> 
-				(request.BeginGetRequestStream, request.EndGetRequestStream, null)
-				.ContinueWith ( task => {
-					using (var requestStream  = task.Result)
-					{
-						requestStream.Write(byteArray, 0, byteArray.Length);
-					}
+            var reqTask = Task.Factory.FromAsync<Stream> 
+                (request.BeginGetRequestStream, request.EndGetRequestStream, null)
+                .ContinueWith ( task => {
+                    using (var requestStream  = task.Result)
+                    {
+                        requestStream.Write(byteArray, 0, byteArray.Length);
+                    }
 
-					var webTask = Task.Factory.FromAsync<WebResponse> 
-						(request.BeginGetResponse, request.EndGetResponse, null)
-						.ContinueWith ( innerTask => {
-							using(var response = (HttpWebResponse)innerTask.Result)
-							using(var stream = response.GetResponseStream ())
-							using(var reader = new StreamReader(response.GetResponseStream()))
-							{
-								if (response.StatusCode != HttpStatusCode.OK){
-									throw new Exception ("500 Error");
-								}
-								return reader.ReadToEnd();
-							}
-						});
-
-
-					if (!webTask.Wait (10000)) {
-						request.Abort ();
-						throw new Exception ("The request timed out");
-					}
+                    var webTask = Task.Factory.FromAsync<WebResponse> 
+                        (request.BeginGetResponse, request.EndGetResponse, null)
+                        .ContinueWith ( innerTask => {
+                            using(var response = (HttpWebResponse)innerTask.Result)
+                            using(var stream = response.GetResponseStream ())
+                            using(var reader = new StreamReader(response.GetResponseStream()))
+                            {
+                                if (response.StatusCode != HttpStatusCode.OK){
+                                    throw new Exception ("500 Error");
+                                }
+                                return reader.ReadToEnd();
+                            }
+                        });
 
 
-					return webTask.Result;
-				});
+                    if (!webTask.Wait (10000)) {
+                        request.Abort ();
+                        throw new Exception ("The request timed out");
+                    }
 
-			if (!reqTask.Wait (10000)) {
-				request.Abort ();
-				throw new Exception ("The request timed out");
-			}
-			return reqTask.Result;
-		}
 
-		public string GetHttp(string baseUri, string api){
+                    return webTask.Result;
+                });
 
-			var url = baseUri + api;
+            if (!reqTask.Wait (10000)) {
+                request.Abort ();
+                throw new Exception ("The request timed out");
+            }
+            return reqTask.Result;
+        }
 
-			var request = WebRequest.Create(url);
-			request.Method = "GET";
+        public string GetHttp(string baseUri, string api){
 
-			var text = string.Empty;
-			var webTask = Task.Factory.FromAsync<WebResponse> 
-				(request.BeginGetResponse, request.EndGetResponse, null)
-				.ContinueWith ( task => {
-					using(var response = (HttpWebResponse)task.Result)
-					using(var stream = response.GetResponseStream ())
-					using(var reader = new StreamReader(response.GetResponseStream()))
-					{
-					if (response.StatusCode != HttpStatusCode.OK){
-						throw new Exception ("500 Error");
-					}
-						text = reader.ReadToEnd();
-					}
-				});
+            var url = baseUri + api;
 
-			if (!webTask.Wait (10000)) {
-				request.Abort ();
-				throw new Exception ("The request timed out");
-			}
+            var request = WebRequest.Create(url);
+            request.Method = "GET";
 
-			return text;
-		}
+            var text = string.Empty;
+            var webTask = Task.Factory.FromAsync<WebResponse> 
+                (request.BeginGetResponse, request.EndGetResponse, null)
+                .ContinueWith ( task => {
+                    using(var response = (HttpWebResponse)task.Result)
+                    using(var stream = response.GetResponseStream ())
+                    using(var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                    if (response.StatusCode != HttpStatusCode.OK){
+                        throw new Exception ("500 Error");
+                    }
+                        text = reader.ReadToEnd();
+                    }
+                });
 
-		public bool IsReadySetupFilter(string check, TestFilter filter){
-			var reader = new StringReader(check);
-			while (reader.Peek () != -1) {
-				var line = reader.ReadLine ();
-				if(line.StartsWith("++READY++")){
-					return true;
-				}else if(line.StartsWith("++INCLUDE++")){
-					filter.Includes.Add (line.Replace ("++INCLUDE++", String.Empty));
-				}else if(line.StartsWith("++EXCLUDE++")){
-					filter.Excludes.Add (line.Replace ("++EXCLUDE++", String.Empty));
-				}
-			}
-			return false;
-		}
+            if (!webTask.Wait (10000)) {
+                request.Abort ();
+                throw new Exception ("The request timed out");
+            }
+
+            return text;
+        }
+
+        public bool IsReadySetupFilter(string check, TestFilter filter){
+            var reader = new StringReader(check);
+            while (reader.Peek () != -1) {
+                var line = reader.ReadLine ();
+                if(line.StartsWith("++READY++")){
+                    return true;
+                }else if(line.StartsWith("++INCLUDE++")){
+                    filter.Includes.Add (line.Replace ("++INCLUDE++", String.Empty));
+                }else if(line.StartsWith("++EXCLUDE++")){
+                    filter.Excludes.Add (line.Replace ("++EXCLUDE++", String.Empty));
+                }
+            }
+            return false;
+        }
+
+        private static Assembly LoadAssembly(string id, string file){
+
+            try{
+#if SILVERLIGHT
+                return Assembly.Load(file);
+#else
+                return Assembly.LoadFile(file);
+#endif
+            }catch(Exception ex){
+                throw new Exception(String.Format("{0} failed to load {1}", id, file),ex);
+            }
+        }
 
         public void Run(string id, string url, string[] dlls)
         {
 
 #if x64
-            if(!Environment.Is64BitProcess)
+            if(!Environment.Is64BitProcess){
                 throw new Exception("This runner is expected to run 64bit");
+            }
+
+#elif x86
+            if(Environment.Is64BitProcess){
+                throw new Exception("This runner is expected to run 32bit");
+            }
 #endif
 
             Console.WriteLine("id:" + id);
@@ -167,15 +186,11 @@ namespace SatelliteRunner.Shared
             Console.WriteLine("==========================");
 
             Console.Write("Connecting..");
-			var connect = GetHttp (url, @"/api/connect/" + id);
-			Console.WriteLine(connect);
+            var connect = GetHttp (url, @"/api/connect/" + id);
+            Console.WriteLine(connect);
             Console.WriteLine("Loading dlls..");
 
-#if SILVERLIGHT
-            var am = dlls.Select(Assembly.Load).ToList();
-#else
-            var am = dlls.Select(Assembly.LoadFile).ToList();
-#endif
+            var am = dlls.Select(f=>LoadAssembly(id, f)).ToList();
 
             Console.WriteLine("Generating Tests...");
 
@@ -183,25 +198,25 @@ namespace SatelliteRunner.Shared
             var runner = Runner.Create(id, am);
 
 
-			TestFilter receivedFilter = new TestFilter();
+            TestFilter receivedFilter = new TestFilter();
 
-			Console.WriteLine("Sending Tests...");
+            Console.WriteLine("Sending Tests...");
 
-			PostHttp (url, @"/api/send_tests", runner.ToListJson ());
+            PostHttp (url, @"/api/send_tests", runner.ToListJson ());
 
-			while (true) {
-				var check = GetHttp (url, @"/api/check_tests");
-				var run = IsReadySetupFilter(check, receivedFilter);
-				if (!run)
-					Thread.Sleep (1000);
-				else
-					break;
-			}
+            while (true) {
+                var check = GetHttp (url, @"/api/check_tests");
+                var run = IsReadySetupFilter(check, receivedFilter);
+                if (!run)
+                    Thread.Sleep (1000);
+                else
+                    break;
+            }
 
             Console.WriteLine("Running Tests...");
 
             runner.RunAll(result => {
-				PostHttp(url, @"/api/send_result", result.ToItemJson());
+                PostHttp(url, @"/api/send_result", result.ToItemJson());
                           }, receivedFilter);
 
             Console.WriteLine("Quiting...");
